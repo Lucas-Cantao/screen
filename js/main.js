ef00379137b5daaa9c146910d3ae98307c7c1b76
Lucas Cantão <124742219+Lucas-Cantao@users.noreply.github.com>
2023-10-28 17:09:06 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,13 +11,47 @@ function InserirFilmesNaTela(filmes) {
     const rating = movie.vote_average.toFixed(1);
     const overview = movie.overview;
 
+    const main = document.createElement('main');
+    main.className = 'main';
+
+
     const section = document.createElement('section');
     section.className = 'cards';
     section.style.background = `url(${posterPath})`;
     section.style.backgroundSize = '170px 250px';
     section.dataset.movieId = movieId;
 
-    ElementoParaInserirFilmes.appendChild(section);
+
+    const contents = document.createElement('div');
+    contents.className = 'contents';
+
+    const nota = document.createElement('div');
+    nota.className = 'nota';
+    const imgStar = document.createElement('img');
+    imgStar.src = '../img/Star.svg';
+    imgStar.width = '15';
+    const nota_text = document.createElement('p');
+    nota_text.className = 'nota-text';
+    nota_text.innerHTML = `${rating}`;
+    
+    
+    const favoritar = document.createElement('div');
+    nota.className = 'favoritar';
+    const imgFav = document.createElement('img');
+    imgFav.src = '../img/Heart.svg';
+    imgFav.width = '15';
+    const favoritar_text = document.createElement('p');
+    favoritar_text.className = 'favoritar-text';
+    favoritar_text.innerHTML = `Favoritar`;
+
+
+
+    main.appendChild(section);
+    nota.appendChild(imgStar);
+    contents.appendChild(nota);
+    contents.appendChild(favoritar);
+    main.appendChild(contents);
+    ElementoParaInserirFilmes.appendChild(main);
   });
 }
 
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let movies = [];
 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR`)
   .then(response => response.json())
   .then(data => {
-    movies = data.results.slice(0, 20); // Atribui o valor a movies, por exemplo (top 10 filmes populares)
+    movies = data.results.slice(0, 10); // Atribui o valor a movies, por exemplo (top 10 filmes populares)
     InserirFilmesNaTela(movies);
   })
   .catch(error => {
@@ -19,3 +19,6 @@ home.addEventListener('click', () => {
   document.getElementById('cabecalho__checkbox').checked = false;
   document.querySelector('.card__lista-vazia').style.display = 'none';
 });
+
+
+// https://api.themoviedb.org/3/search/movie?query=f&api_key=5d93ad67ab4499fded984604a2f5c80b&language=pt-BR
